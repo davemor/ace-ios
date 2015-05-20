c706b16d6c29b732c3c8d7d86bbda4d3c05ad81b
David Morrison <me@davemor.com>
2015-05-20 08:41:40 +0100

    Add CVCalendar as a library.

---
+++ ACE/CalendarViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CalendarViewController.swift
+//  ACE
+//
+//  Created by David Morrison on 20/05/2015.
+//  Copyright (c) 2015 David Morrison. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func back(sender: UIBarButtonItem) {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: {})
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ACE/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  ACE
-//
-//  Created by David Morrison on 19/05/2015.
-//  Copyright (c) 2015 David Morrison. All rights reserved.
-//
-
-import Foundation
